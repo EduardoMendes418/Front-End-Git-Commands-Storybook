fd37134e06cb004e177a2da922de5d55bc9406e1
eduardo <eduardomendes418@gmail.com>
2025-11-17 07:19:46 -0300

    [FDS-08] 🏗 feat - add Basic

---
+++ src/stories/CommandCard.stories.jsx
@@ -0,0 +1,46 @@
+import CommandCard from '../components/CommandCard';
+
+export default {
+  title: 'Git Commands/CommandCard',
+  component: CommandCard,
+  parameters: {
+    layout: 'padded',
+  },
+  tags: ['autodocs'],
+};
+
+export const Basic = {
+  args: {
+    command: 'git init',
+    description: 'Criar um novo repositório.',
+    usage: 'git init',
+    example: 'git init'
+  },
+};
+
+export const WithExample = {
+  args: {
+    command: 'git commit -m',
+    description: 'Salvar suas alterações com segurança.',
+    usage: 'git commit -m "mensagem"',
+    example: 'git commit -m "Adiciona nova funcionalidade"'
+  },
+};
+
+export const ComplexCommand = {
+  args: {
+    command: 'git log --oneline --graph --all',
+    description: 'Ver histórico de commits resumido e visual.',
+    usage: 'git log --oneline --graph --all',
+    example: 'git log --oneline --graph --all'
+  },
+};
+
+export const CloneCommand = {
+  args: {
+    command: 'git clone',
+    description: 'Copiar um repositório existente.',
+    usage: 'git clone <url>',
+    example: 'git clone https://github.com/user/repo.git'
+  },
+};
